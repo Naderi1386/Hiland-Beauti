8e4cb7ac4c4994e702cbe6db3643311a31893080
unknown <mh.naderi1386@gmail.com>
2025-02-20 23:15:43 +0330

    making favorites list responsive

---
+++ src/feature/favorites/FavoriteItems.tsx
@@ -0,0 +1,58 @@
+import { useNavigate } from "react-router";
+import { ProductType } from "../shop/ProductType";
+import { IoMdClose } from "react-icons/io";
+import { useDispatch } from "react-redux";
+import { removeFavorite } from "../../redux/Favorite";
+
+interface FavoriteItemsPropsType {
+  product: ProductType;
+}
+
+const FavoriteItems = ({ product }: FavoriteItemsPropsType) => {
+  const { id, title, img, discount, price, mainPrice } = product;
+  const navigate = useNavigate();
+  const onClick = () => navigate(`/product/${id}`);
+  const dispatch=useDispatch()
+  return (
+    <div className="w-full sm:w-[48%] lg:w-[32%] xl:w-[22.5%]">
+      <div className="flex items-center justify-between gap-2">
+        <div onClick={()=>dispatch(removeFavorite(id))} className="flex items-center cursor-pointer transition-all duration-150 hover:opacity-70">
+          <IoMdClose />
+          <span>حذف</span>
+        </div>
+      </div>
+
+      <li
+        className="cursor-pointer  shadow-custome6 rounded-md mt-2"
+        onClick={onClick}
+      >
+        <div className="bg-gray-400 rounded-t-lg overflow-hidden relative cursor-pointer">
+          <img src={img} alt={`img/${title}`} className="w-full" />
+          <span className="absolute top-2 right-2 bg-customePink-500 font-bold text-xs flex justify-center items-center text-white w-[35px] h-[35px] lg:w-[45px] lg:h-[45px] rounded-[100%]">
+            {discount}
+
+            {"%"}
+            {"-"}
+          </span>
+        </div>
+        <div className="py-3 px-2">
+          <h3 className="text-center mb-8 transition-all duration-200 hover:opacity-60 cursor-pointer text-sm lg:text-base">
+            {title}
+          </h3>
+          <div className="flex flex-col lg:flex-row items-center justify-center gap-[.3rem] md:text-[.69rem] lg:text-xs xl:text-sm">
+            <span className="text-stone-300 line-through flex items-center">
+              <span>{price}</span>
+              <span>{"تومان"}</span>
+            </span>
+            <span className="font-bold text-customePink-500 flex items-center">
+              <span>{mainPrice}</span>
+              <span>{"تومان"}</span>
+            </span>
+          </div>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default FavoriteItems;
--- src/feature/favorites/FavoritesList.tsx
+++ src/feature/favorites/FavoritesList.tsx
@@ -1,13 +1,24 @@
-import { useSelector } from "react-redux"
-import { getFavorites, StoreType } from "../../redux/Store"
+import { useSelector } from "react-redux";
+import { getFavorites, StoreType } from "../../redux/Store";
 import { ProductType } from "../shop/ProductType";
+import FavoriteItems from "./FavoriteItems";
 
 const FavoritesList = () => {
-    const products=useSelector<StoreType>(getFavorites) as ProductType[]
-    
+  const products = useSelector<StoreType>(getFavorites) as ProductType[];
+  const isTwo = products.length === 3 || products.length === 2;
   return (
-    <div>FavoritesList</div>
-  )
-}
+    <div>
+      <ul
+        className={`mt-4 px-6 flex gap-6 items-center ${
+          !isTwo && "justify-between"
+        }  flex-wrap`}
+      >
+        {products.map((product) => (
+          <FavoriteItems key={product.id} product={product} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default FavoritesList
+export default FavoritesList;
