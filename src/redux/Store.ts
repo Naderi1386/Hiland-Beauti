32d89983d3aa83cbe322b5d0e9e831fc0d67fda6
unknown <mh.naderi1386@gmail.com>
2025-02-21 17:29:53 +0330

    add a simple selector for cart items

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,7 +1,7 @@
 import storage from "redux-persist/lib/storage";
 import { persistStore, persistReducer } from "redux-persist";
 import favoritesReducer, { FavoritesStateType } from "./Favorite";
-import cartReducer from "./Cart";
+import cartReducer, { CartStateType } from "./Cart";
 import { configureStore } from "@reduxjs/toolkit";
 const persistConfigFavorites = {
   key: "favoritesProducts",
@@ -37,6 +37,7 @@ export type StoreType = ReturnType<typeof store.getState>;
 
 interface SelectorType {
   favorites: FavoritesStateType;
+  cart: CartStateType;
 }
 
 export const getFavorites = (store: SelectorType) => store.favorites.favorites;
@@ -45,3 +46,5 @@ export const getFavorite = (id: string) => (store: StoreType) =>
   store.favorites.favorites.find((fav) => fav.id === id);
 export const getFavoritesCount = (store: SelectorType) =>
   store.favorites.favorites.length;
+
+export const getCartItems = (store: SelectorType) => store.cart.cart;
