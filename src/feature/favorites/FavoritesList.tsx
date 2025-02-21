9652fde3b6aef2c82fa9a6e1e4cdb0f2703df3da
unknown <mh.naderi1386@gmail.com>
2025-02-21 18:10:29 +0330

    changing ui

--- src/feature/favorites/FavoritesList.tsx
+++ src/feature/favorites/FavoritesList.tsx
@@ -3,7 +3,7 @@ import { DispatchType, getFavorites, StoreType } from "../../redux/Store";
 import { ProductType } from "../shop/ProductType";
 import FavoriteItems from "./FavoriteItems";
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { IoMdClose } from "react-icons/io";
 import { useDispatch } from "react-redux";
 import { removeAll, removeSomeFavorites } from "../../redux/Favorite";
@@ -22,44 +22,60 @@ const FavoritesList = () => {
     dispatch(removeAll());
     setSelected([]);
   };
-  if(products.length===0) return <div className="text-center pt-8 pb-12">
-    <h2 className="text-lg font-bold mb-2 ">محصولی در لیست علاقه مندی شما وجود ندارد</h2>
-    <Link to={'/shop'} className="text-sm underline text-customePink-500">مشاهده محصولات</Link>
-
-  </div>
+  if (products.length === 0)
+    return (
+      <div className="text-center pt-8 pb-20">
+        <h2 className="text-lg font-bold mb-2 ">
+          محصولی در لیست علاقه مندی شما وجود ندارد
+        </h2>
+        <Link to={"/shop"} className="text-sm underline text-customePink-500">
+          مشاهده محصولات
+        </Link>
+      </div>
+    );
 
   return (
     <div>
       {selected.length > 0 && (
-        <motion.div
-          variants={{
-            hidden: { opacity: 0, y: -10 },
-            visible: {
-              opacity: 1,
-              y: 0,
-              transition: { duration: 0.5, ease: "easeOut" },
-            },
-          }}
-          initial="hidden"
-          animate="visible"
-        >
-          <div className="bg-gray-100 py-2 px-3 flex items-center gap-4 justify-start">
-            <div
-              onClick={onRemove}
-              className="inline-flex items-center cursor-pointer transition-all duration-150 hover:opacity-70 "
+        <AnimatePresence>
+          {selected.length > 0 && (
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, y: -10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 0.5, ease: "easeOut" },
+                },
+                exit: {
+                  opacity: 0,
+                  y: -10,
+                  transition: { duration: 0.5, ease: "easeIn" },
+                },
+              }}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
             >
-              <IoMdClose />
-              <span>حذف</span>
-            </div>
-            <div
-              onClick={onRemoveAll}
-              className="inline-flex items-center cursor-pointer transition-all duration-150 hover:opacity-70 "
-            >
-              <IoMdClose />
-              <span>حذف همه</span>
-            </div>
-          </div>
-        </motion.div>
+              <div className="bg-gray-100 py-2 px-3 flex items-center gap-4 justify-start">
+                <div
+                  onClick={onRemove}
+                  className="inline-flex items-center cursor-pointer transition-all duration-150 hover:opacity-70"
+                >
+                  <IoMdClose />
+                  <span>حذف</span>
+                </div>
+                <div
+                  onClick={onRemoveAll}
+                  className="inline-flex items-center cursor-pointer transition-all duration-150 hover:opacity-70"
+                >
+                  <IoMdClose />
+                  <span>حذف همه</span>
+                </div>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       )}
       <ul
         className={`mt-4 px-6 flex gap-6 items-center ${
