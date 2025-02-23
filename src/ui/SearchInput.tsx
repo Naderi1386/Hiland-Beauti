50a4a110bea4fea067a584e7c5f5568388502544
unknown <mh.naderi1386@gmail.com>
2025-02-23 23:06:53 +0330

    add a search category for out header form

--- src/ui/SearchInput.tsx
+++ src/ui/SearchInput.tsx
@@ -1,24 +1,42 @@
 import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
+import { useNavigate } from "react-router";
+
+const categories = ["مو", "آرایشی", "بهداشتی", "ارایشی"];
 
 const SearchInput = () => {
-    const [input,setInput]=useState('')
-    const onSubmit=()=>{
-      if(input){
-        setInput('')
-      }
-    }
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+  const onSubmit = () => {
+    const category = categories.find(
+      (category) => category === input || category.startsWith(input)
+    );
+    if (category === "آرایشی" || category === "ارایشی")
+      navigate(`/shop?category=آرایشی`);
+    else if (category) navigate(`/shop?category=${category}`);
+    setInput("")
+  };
   return (
-    <form onSubmit={(e)=>{
-        e.preventDefault()
-        onSubmit()
-    }} className="hidden lg:flex w-[410px] border border-solid border-stone-200 rounded-md overflow-hidden h-[45px]">
-      <input value={input} onChange={(e)=>setInput(e.target.value)} type="text" placeholder="جستجوی محصولات" className="w-[90%] font-light focus:outline-none text-[.85rem] px-3" />
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+      }}
+      className="hidden lg:flex w-[410px] border border-solid border-stone-200 rounded-md overflow-hidden h-[45px]"
+    >
+      <input
+        required
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+        placeholder="نام محصول و نوع محصول و ..."
+        className="w-[90%] font-light focus:outline-none text-[.85rem] px-3"
+      />
       <button className="w-[10%] bg-customePink-500 flex justify-center items-center h-full">
         <IoIosSearch size={25} fill="white" />
       </button>
     </form>
   );
-}
+};
 
-export default SearchInput
+export default SearchInput;
--- src/ui/ShopPage.tsx
+++ src/ui/ShopPage.tsx
@@ -1,25 +1,31 @@
 import { useNavigate, useSearchParams } from "react-router";
 import ShopLeftSide from "../feature/shop/ShopLeftSide";
 import ShopRightSide from "../feature/shop/ShopRightSide";
-import PageTitle from "./PageTitle"
+import PageTitle from "./PageTitle";
 import { MdArrowRightAlt } from "react-icons/md";
 
 const ShopPage = () => {
-  const [searchParams]=useSearchParams()
-  const navigate=useNavigate()
-  const category=searchParams.get('category') as string
-  const text=category ? category : 'فروشگاه'
-  const icon=category ? <MdArrowRightAlt className="cursor-pointer" size={20} onClick={()=>navigate(-1)}/> : null
-  
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const category = searchParams.get("category") as string;
+  const text = category ? category : "فروشگاه";
+  const icon = category ? (
+    <MdArrowRightAlt
+      className="cursor-pointer"
+      size={20}
+      onClick={() => navigate(-1)}
+    />
+  ) : null;
+
   return (
     <div>
-      <PageTitle title={text} icon={icon}   />
+      <PageTitle title={text} icon={icon} />
       <div className="flex gap-14 items-start">
         <ShopRightSide />
         <ShopLeftSide />
       </div>
     </div>
   );
-}
+};
 
-export default ShopPage
+export default ShopPage;
